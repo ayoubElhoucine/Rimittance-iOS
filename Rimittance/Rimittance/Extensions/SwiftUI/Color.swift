f11ae83e16f79c6552116aad27604f9be820e011
Elhoucine Ayoub <ayoubelhoucine@gmail.com>
2024-02-01 21:35:17 +0100

    add all possible colors

--- Rimittance/Rimittance/Extensions/SwiftUI/Color.swift
+++ Rimittance/Rimittance/Extensions/SwiftUI/Color.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+@available(iOS 13.0, *)
+public extension Color {
+    
+    // Primary color
+    static let primary100 = Color(red: 0, green: 117/255, blue: 84/255)
+    static let primary70 = Color(red: 8/255, green: 160/255, blue: 117/255)
+    static let primary05 = Color(red: 237/255, green: 248/255, blue: 245/255)
+    static let primary15 = Color(red: 200/255, green: 234/255, blue: 225/255)
+    static let primary01 = Color(red: 248/255, green: 252/255, blue: 251/255)
+    static let walletGreen1 = Color(red: 0, green: 168/255, blue: 90/255)
+    static let walletGreen2 = Color(red: 5/255, green: 92/255, blue: 0)
+
+    // grey colors
+    static let grey100 = Color(red: 0, green: 18/255, blue: 44/255)
+    static let grey70 = Color(red: 64/255, green: 77/255, blue: 97/255)
+    static let grey50 = Color(red: 127/255, green: 136/255, blue: 149/255)
+    static let grey25 = Color(red: 191/255, green: 195/255, blue: 202/255)
+    static let grey15 = Color(red: 224/255, green: 226/255, blue: 229/255)
+    static let grey05 = Color(red: 242/255, green: 243/255, blue: 244/255)
+
+    // accent
+    static let accent100 = Color(red: 1, green: 207/255, blue: 0)
+
+    // blue colors
+    static let blue100 = Color(red: 27/255, green: 152/255, blue: 224/255)
+    static let blue10 = Color(red: 234/255, green: 246/255, blue: 252/255)
+
+    static let red = Color(red: 177/255, green: 12/255, blue: 12/255)
+    
+}
+
